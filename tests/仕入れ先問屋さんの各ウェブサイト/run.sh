5616250b4603e6f9cc8f838336ee5bdb70b4daf5
kenken999 <kenken999@users.noreply.huggingface.co>
2025-06-06 08:42:18 +0200

    update

--- tests/仕入れ先問屋さんの各ウェブサイト/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Install dependencies
-python -m pip install -r requirements.txt
-
-# Run main script
-python main.py
--- tests/語学留学を希望する学生および社会人向け/database.py
+++
@@ -1,7 +0,0 @@
-from app import db
-
-def create_database():
-    db.create_all()
-
-def query_database(query):
-    return db.session.execute(query)
